c065616acb687635bff31a1902311258749c4667
Ryan Pavlik <ryan@sensics.com>
2015-02-09 14:05:09 -0600

    Dramatically simplify, and install to the default location in chocolatey.

--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -1,38 +1,27 @@
-﻿#NOTE: Please remove any commented lines to tidy up prior to releasing the package, including this one
-
-$packageName = 'OpenCV' # arbitrary name for the package, used in messages
-$binRoot = "$env:systemdrive\"
-$installDir = Join-Path $binRoot 'OpenCV24'
+﻿$packageName = 'OpenCV'
+$binaryKeepers = 'x86\vc10' # All other binaries will be ignored
+$scriptPath = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
+$toolsDir = $scriptPath
+$downloadedFile = Join-Path $scriptPath 'opencv.exe'
 $url = 'http://downloads.sourceforge.net/project/opencvlibrary/opencv-win/2.4.10/opencv-2.4.10.exe'
-#$url = 'http://downloads.sourceforge.net/project/opencvlibrary/opencv-win/2.4.0/OpenCV-2.4.0.exe' # download url
-$url64 = $url # 64bit URL here or just use the same as $url
-$validExitCodes = @(0) #please insert other valid exit codes here, exit codes for ms http://msdn.microsoft.com/en-us/library/aa368542(VS.85).aspx
 
-try { #error handling is only necessary if you need to do anything in addition to/instead of the main helpers
-	write-host "Chocolatey is installing OpenCV to $installDir"
-	write-host "Please wait..."
+Get-ChocolateyWebFile $packageName $downloadedFile $url
 
-	$tempDir = "$env:TEMP\chocolatey\opencv"
-	if (![System.IO.Directory]::Exists($tempDir)) {[System.IO.Directory]::CreateDirectory($tempDir)}
-	$tempFile = Join-Path $tempDir "opencv.exe"
-	
-	Get-ChocolateyWebFile 'OpenCV' "$tempFile" "$url"
-	
-	write-host "Extracting the contents of $tempFile to $installDir"
-	
-	Start-Process "7za" -ArgumentList "x -o`"$installDir`" -y `"$tempFile`"" -Wait
-#	Install-ChocolateyPath "$installDir\opencv\build\x86\vc10\bin" 'User'
-	
-	if (Test-Path $tempFile){
-		Remove-Item $tempFile
-	}
-  #$processor = Get-WmiObject Win32_Processor
-  #$is64bit = $processor.AddressWidth -eq 64
+Get-ChocolateyUnzip $downloadedFile $toolsDir '' $packageName
 
-  
-  # the following is all part of error handling
-  Write-ChocolateySuccess "$packageName"
+function Create-IgnoreFile($exeName) {
+    $ignoreFile = "$exeName.ignore"
+    set-content -Path ($ignoreFile) -Value ($null)
+}
+
+try {
+    Create-IgnoreFile $downloadedFile
+
+    # Ignore all but one set of binaries.
+    $unusedBinaries = Get-ChildItem -Path "$toolsDir\*.exe" -Recurse -File | Where-Object {!($_.DirectoryName -like "*build\$binaryKeepers\bin*")} | Select-Object -Property FullName
+    foreach ($exeFile in $unusedBinaries) {
+        Create-IgnoreFile $exeFile.FullName
+    }
 } catch {
-  Write-ChocolateyFailure "$packageName" "$($_.Exception.Message)"
-  throw 
+  throw $_.Exception
 }
